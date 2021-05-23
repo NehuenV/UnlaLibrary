9940fcbbdff902ef06d71808f23e6d5ece9087c1
gazapus <cristianv129@hotmail.com>
2021-05-23 19:29:26 -0300

    Agregada función de ajax en agregar alumnos

--- UnlaLibrary.Data/Interface/IProfesoresRepository.cs
+++ UnlaLibrary.Data/Interface/IProfesoresRepository.cs
@@ -8,12 +8,12 @@ namespace UnlaLibrary.Data.Interface
 {
     public interface IProfesoresRepository
     {
-
+        List<Universidad> GetUniverdadesByUsuario(int iduser);
+        List<Carrera> GetCarrerasByUniversidadAndUser(int iduser, int iduniversidad);
+        List<Materia> GetMateriasByUserAndCarreraAndUniversidad(int iduser, int idcarrera, int iduniversidad);
         List<Universidad> GetUniverdades();
         List<UsuarioCarreraUniversidad> GetUsuariosByCarreraUniversidad(int iduser, int iduni);
-
         List<Materia> GetMateriasByCarrera(int idcarr);
-
         List<Alumno> GetAlumnosByCarrera(int idMat, int idcarr);
         List<Alumno> GetAlumnosAgregadosMateria(int idMat);
         List<Alumno> GetAlumnosNoagregadosByCarrera(int idMat);
